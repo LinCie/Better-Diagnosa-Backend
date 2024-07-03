7bad018c3ff217127853a5be78bf90eb3d47b4f9
LinCie <aldiofernanda@protonmail.com>
2024-07-03 21:23:54 +0700

    add getUser function

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,20 @@
+import {
+  CreateRequestContext,
+  EntityManager,
+  MikroORM,
+} from '@mikro-orm/sqlite';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/User.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly orm: MikroORM,
+    private readonly em: EntityManager,
+  ) {}
+
+  @CreateRequestContext()
+  async getUser(username: string) {
+    return await this.em.findOne(User, { username: username });
+  }
+}
