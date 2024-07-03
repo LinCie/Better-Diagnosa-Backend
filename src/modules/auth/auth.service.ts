b516b38cd889f3b5aab52c237a477fd18bcc613a
LinCie <aldiofernanda@protonmail.com>
2024-07-03 22:00:45 +0700

    added validate user function

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,22 @@
+import { CreateRequestContext, MikroORM } from '@mikro-orm/sqlite';
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly orm: MikroORM,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @CreateRequestContext()
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.getUser(username);
+    const passwordCorrect = await bcrypt.compare(password, user.hash);
+    if (passwordCorrect) {
+      return { username: user.username, id: user.id };
+    }
+    return null;
+  }
+}
