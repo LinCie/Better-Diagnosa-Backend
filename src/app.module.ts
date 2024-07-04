e44ef8fc0756be842a1e57909ff96656a1dc8d71
LinCie <aldiofernanda@protonmail.com>
2024-07-04 20:58:26 +0700

    added a few stuffs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, MikroOrmModule.forRoot()],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MikroOrmModule.forRoot(),
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
