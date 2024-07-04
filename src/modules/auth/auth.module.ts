697db88d36c48ca55548828130a240d27a969db6
LinCie <aldiofernanda@protonmail.com>
2024-07-04 21:26:58 +0700

    added jwt guards

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     PassportModule,
