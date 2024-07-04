a622023017249b9d021d2aeb8b149bf3aea03794
LinCie <aldiofernanda@protonmail.com>
2024-07-05 00:42:22 +0700

    modify validate function

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -2,10 +2,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { EntityManager, MikroORM } from '@mikro-orm/sqlite';
+import { User } from '@/entities/User.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly orm: MikroORM,
+    private readonly em: EntityManager,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +19,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { id: payload.sub, username: payload.username };
+  async validate(payload: { sub: number; username: string }) {
+    const user = await this.em.findOne(User, { id: payload.sub });
+    if (user) {
+      return user;
+    }
+    return null;
   }
 }
