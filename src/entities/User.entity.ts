64c26c5dbbecd02cc848db74f32fb068c3f2b62b
LinCie <aldiofernanda@protonmail.com>
2024-07-04 23:12:42 +0700

    added roles to user

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -11,6 +11,9 @@ export class User extends BaseEntity {
   @Property()
   hash!: string;
 
+  @Property()
+  roles: string[];
+
   @OneToOne(() => History, (history) => history.user, {
     owner: true,
     orphanRemoval: true,
