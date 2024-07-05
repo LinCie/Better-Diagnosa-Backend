264c307b5e48e653ba01b81f47c256044449a2c3
LinCie <aldiofernanda@protonmail.com>
2024-07-06 06:41:58 +0700

    added post and get

--- src/modules/history/history.controller.ts
+++ src/modules/history/history.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { HistoryService } from './history.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RequestWithUser } from '../auth/auth.interface';
 
 @Controller('history')
-export class HistoryController {}
+export class HistoryController {
+  constructor(private readonly historyService: HistoryService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getHistory(@Request() req: RequestWithUser) {
+    return this.historyService.getHistory(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async addHistory(
+    @Request() req: RequestWithUser,
+    @Body() body: { isDengue: boolean },
+  ) {
+    return this.historyService.addHistory(req.user, body.isDengue);
+  }
+}
--- src/modules/history/history.service.ts
+++ src/modules/history/history.service.ts
@@ -1,4 +1,25 @@
+import { User } from '@/entities/User.entity';
+import {
+  CreateRequestContext,
+  EntityManager,
+  MikroORM,
+} from '@mikro-orm/sqlite';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+  constructor(
+    private readonly orm: MikroORM,
+    private readonly em: EntityManager,
+  ) {}
+
+  @CreateRequestContext()
+  async getHistory(user: User) {
+    return user.history;
+  }
+
+  async addHistory(user: User, isDengue: boolean) {
+    user.history.diagnosis.push({ isDengue, time: Date.now() });
+    return user.history;
+  }
+}
