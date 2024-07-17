3ad4f6ba0b1bca2dc0165504c2c19aa54bb17df3
LinCie <aldiofernanda@protonmail.com>
2024-07-17 22:07:12 +0700

    remove old modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthModule as AuthModuleOld } from './modules/auth/auth.module';
-import { UsersModule as UsersModuleOld } from './modules/users/users.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ConfigModule } from '@nestjs/config';
-import { QuestionModule } from './modules/question/question.module';
-import { HistoryModule } from './modules/history/history.module';
 import { UsersModule } from './v1/users/users.module';
 import { AuthModule } from './v1/auth/auth.module';
 import { HistoriesModule } from './v1/histories/histories.module';
 import { QuestionsModule } from './v1/questions/questions.module';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(),
-    UsersModuleOld,
     UsersModule,
-    AuthModuleOld,
     AuthModule,
-    QuestionModule,
-    HistoryModule,
     HistoriesModule,
     QuestionsModule,
   ],
