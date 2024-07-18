afe5d83e12c8b8127f25b4e6253bad8ecf006239
LinCie <aldiofernanda@protonmail.com>
2024-07-18 21:49:12 +0700

    added static

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './v1/users/users.module';
 import { AuthModule } from './v1/auth/auth.module';
 import { HistoriesModule } from './v1/histories/histories.module';
 import { QuestionsModule } from './v1/questions/questions.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,6 +16,10 @@ import { QuestionsModule } from './v1/questions/questions.module';
     AuthModule,
     HistoriesModule,
     QuestionsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
   ],
   controllers: [],
   providers: [],
